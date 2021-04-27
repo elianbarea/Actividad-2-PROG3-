ea87a4029487a03e9932389a0bf85095538fd9c3
elianbarea <67470354+elianbarea@users.noreply.github.com>
2021-04-27 14:09:30 -0300

    Primer modificacion (Elian Barea)
    MODIFIQUE LA CLASE ARTICULO:
    La hice publica, y hice publico los gets y sets de sus variables.
    la movi a la libreria DOMINIO.
    -------------------------------------
    CREE LA LIBRERIA DOMINIO Y NEGOCIO:
    referencie NEGOCIO con DOMINIO
    referencie TP2 PROG 3 con DOMINIO y NEGOCIO
    cree la clase AccesoDatos y LocalNegocio
    ---------------------------------------------
    CONEXION A SQL:
    intente la conexion a sql y me encontre con un error, subo el update para que mi compañero pueda darme una mano y depaso quede constancia con las actualizaciones

---
+++ Actividad-2-PROG3-.1.0/Dominio/Articulo.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Articulo
+    {
+        public string Cod_articulo { get; set; }
+        public string Nombre {get; set;}
+        public string Descripcion { get; set; }
+        public string Marca { get; set; }
+        public string Categoria { get; set; }
+        public string Imagen { get; set; }
+        public int Precio { get; set; }
+
+
+        /*private string Cod_articulo;
+        private string Nombre;
+        private string Descripcion;
+        private string Marca;
+        private string Categoria;
+        private string Imagen;
+        private int Precio;
+
+        public string cod_articulo
+        {
+            get {return Cod_articulo; }
+            set { Cod_articulo=value; } 
+        }
+        public string nombres
+        {
+            get { return Nombre; }
+            set { Nombre = value; }
+        }
+        public string descripcion
+        {
+            get { return descripcion; }
+            set { descripcion = value; }
+        }
+        public string marca
+        {
+            get { return marca; }
+            set { marca = value; }
+        }
+        public string categoria
+        {
+            get { return categoria; }
+            set { categoria= value; }
+        }
+        public string imagen
+        {
+            get { return imagen; }
+            set { imagen = value; }
+        }
+        public int precio
+        {
+            get { return precio; }
+            set { precio = value; }
+        }*/
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Actividad-2-PROG3-.1.0/Negocio/localNegocio.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio;
+
+namespace Negocio
+{
+    public class localNegocio
+    {
+
+        public List<Articulo> listar()
+        {
+            
+            List<Articulo> lista = new List<Articulo>();
+            AccesoDatos datos = new AccesoDatos();
+            
+            try
+            {
+                datos.setearConsulta("select codigo, Nombre, Descripcion,  precio from ARTICULOS");
+                datos.ejecutarlectura();
+
+                while (datos.Lector.Read())
+                {
+                    Articulo ar = new Articulo();
+                    ar.Cod_articulo = (string)datos.Lector["Codigo"];
+                    ar.Nombre = (string)datos.Lector["nombre"];
+                    ar.Descripcion = (string)datos.Lector["descripcion"];
+                    ar.Precio = (int)datos.Lector["precio"];
+
+                    lista.Add(ar);
+
+                    
+                   
+
+                }
+                    return lista;
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+
+
+
+
+            
+
+        }
+
+
+
+    }
+}
+
